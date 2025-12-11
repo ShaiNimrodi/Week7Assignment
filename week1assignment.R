f97bddcc7c42d5260936b05b28674ee45c35928f
ShaiNimrodi <shainimrodi@gmail.com>
2025-12-11 11:50:08 +0200

    Week 1 Update
    Updated Week 1 Assignment

--- week1assignment.R
+++ week1assignment.R
@@ -3,7 +3,6 @@
 # assignment by Shai Nimrodi 318516465
 
 participant_id = seq(1, 6, 1)
-rm(x)
 
 gender = c('male', 'female', 'female', 'female', 'male', 'male')
 
@@ -11,10 +10,24 @@ age = c(15, 20, 25, 32, 36, 40)
 
 depression = c(1, 0, 0, 1, 1, 0)
 
-df = data.frame(participant_id, gender, age, depression)
+df_participants = data.frame(participant_id, gender, age, depression)
 print(df)
 
 View(df)
+
+library(dplyr)
+
+df_participants |>
+  summarize(
+    mean_age = mean(age),
+    num_female = sum(gender == "female"),
+    num_male = sum(gender == "male")
+  )
+
+library(ggplot2)
+
+ggplot(df, aes(x = gender, y = age))+
+  geom_point()
 
 write.csv(df, file = '~/Desktop/R course/Week 1- Intro/df.csv')
 
